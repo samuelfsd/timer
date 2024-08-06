f6d62e150775b28c82b6ce5d65a932e66ed714fc
samuelfsd <samuel.estudo778@gmail.com>
2024-08-06 10:32:45 -0300

    feat: add color scheme and fonts

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -4,8 +4,8 @@ export const ButtonContainer = styled.button`
   width: 100px;
   height: 40px;
 
-  background-color: ${props => props.theme.primary};
-  color: aliceblue;
+  background-color: ${({theme}) => theme["green-500"]};
+  color:  ${({theme}) => theme.white};
   border: 0;
   margin-right: 8px;
   border-radius: 8px;
