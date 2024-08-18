b9d3bd9727817f787273fd3161faa876e05c827a
samuelfsd <samuel.estudo778@gmail.com>
2024-08-18 12:28:06 -0300

    refactor: create component countdown

---
+++ src/pages/Home/Countdown/index.tsx
@@ -0,0 +1,75 @@
+import { useContext, useEffect } from 'react'
+
+import { differenceInSeconds } from 'date-fns'
+
+import { CountdownContainer, Separator } from './styles'
+import { CyclesContext } from '..'
+
+export function Countdown() {
+  const {
+    activeCycle,
+    activeCycleId,
+    markCurrentCycleAsFinished,
+    amountSecondsPassed,
+    setSecondsPassed,
+  } = useContext(CyclesContext)
+
+  const totalSecondsCycle = activeCycle ? activeCycle.minutesAmount * 60 : 0
+
+  useEffect(() => {
+    let interval: number
+
+    if (activeCycle) {
+      interval = setInterval(() => {
+        const secondsDiff = differenceInSeconds(
+          new Date(),
+          activeCycle.startDate,
+        )
+
+        if (secondsDiff >= totalSecondsCycle) {
+          markCurrentCycleAsFinished()
+          setSecondsPassed(totalSecondsCycle)
+          clearInterval(interval)
+        } else {
+          setSecondsPassed(secondsDiff)
+        }
+      }, 1000)
+    }
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [
+    activeCycle,
+    totalSecondsCycle,
+    activeCycleId,
+    markCurrentCycleAsFinished,
+    setSecondsPassed,
+  ])
+
+  const currentSeconds = activeCycle
+    ? totalSecondsCycle - amountSecondsPassed
+    : 0
+
+  const minutesAmount = Math.floor(currentSeconds / 60)
+  const secondsAmount = currentSeconds % 60
+
+  const minutes = String(minutesAmount).padStart(2, '0')
+  const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [activeCycle, minutes, seconds])
+
+  return (
+    <CountdownContainer>
+      <span>{minutes[0]}</span>
+      <span>{minutes[1]}</span>
+      <Separator>:</Separator>
+      <span>{seconds[0]}</span>
+      <span>{seconds[1]}</span>
+    </CountdownContainer>
+  )
+}
