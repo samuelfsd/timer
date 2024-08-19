5edd5d76623d45ec8d51e3db9e1ed88e86a771ba
samuelfsd <samuel.estudo778@gmail.com>
2024-08-19 18:32:58 -0300

    feat: add context to all routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,15 @@ import { GlobalStyle } from './styles/global'
 import { Router } from './Router'
 import { BrowserRouter } from 'react-router-dom'
 
+import { CyclesContextProvider } from './contexts/CyclesContexts'
+
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
 
       <GlobalStyle />
